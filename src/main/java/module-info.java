2835dc6c3255115c9d5de481b44235873ae87a64
Redblutac1 <leemenchanbbt10@gmail.com>
2022-10-02 16:44:02 +0800

    Added data processing capabilities

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.demo {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.web;
-
-    opens com.example.demo to javafx.fxml;
-    exports com.example.demo;
-}
